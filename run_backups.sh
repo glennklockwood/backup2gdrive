96bbbb3a35a354bdd1b997583ed303ebf46a780d
Glenn K. Lockwood <glennklockwood@gmail.com>
2020-04-04 21:40:31 -0700

    timestamp each line of logfile

--- run_backups.sh
+++ run_backups.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+#
+# Wrapper for backup_mud.py.  Insert into crontab, e.g.,
+#
+#     @daily source /home/mud/backup/google-api/bin/activate && /home/mud/backup/run_backups.sh
+#
 
 HERE=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 MUD_ROOT=$HOME
@@ -11,5 +16,5 @@ do
 	--client-secrets "${HERE}/credentials.json" \
 	--backup-dir "Mud Backups" \
 	--backup-prefix "mud_${port}" \
-	${port} >> "${HERE}/backups.log"
+	${port} | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> "${HERE}/backups.log"
 done
