7d888e836761310a62b2782720f455842ea270a3
Glenn K. Lockwood <glennklockwood@gmail.com>
2020-04-07 12:39:52 -0700

    add port 1315

--- run_backups.sh
+++ run_backups.sh
@@ -8,8 +8,11 @@
 HERE=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 MUD_ROOT=$HOME
 
-for port in 1313 1314 1316
+for port in 1313 1314 1315 1316
 do
+    if [ ! -d "${port}" ]; then
+	echo "Port ${port} does not exist; skipping" | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> "${HERE}/backups.log"
+    fi
     cd "$MUD_ROOT" && \
     ${HERE}/backup_mud.py \
         --token-file "${HERE}/token.pickle" \
